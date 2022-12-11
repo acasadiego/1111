95f7b8a433a5dd9dc9841ad56a288393fdbb33cd
Andrés Eduardo Casadiegos Gómez <andres.casadiegos@norsoft.com.co>
2022-12-11 18:50:50 -0500

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+function bloque2() {
+    let bloque1 = document.getElementById('bloque-1');
+    let bloque2 = document.getElementById('bloque-2');
+
+    bloque1.style.display = "none";
+    bloque2.style.display = "block";
+}
+
+function bloque3() {
+    let bloque2 = document.getElementById('bloque-2');
+    let bloque3 = document.getElementById('bloque-3');
+
+    bloque2.style.display = "none";
+    bloque3.style.display = "block";
+}
